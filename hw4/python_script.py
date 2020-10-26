5e862fdf2c29712f50d235d45e74f134a108b4dd
KKondratenya <kondratenya21@yahoo.com>
2020-10-26 23:51:47 +0300

    add json parameter

--- hw4/python_script.py
+++ hw4/python_script.py
@@ -1,5 +1,7 @@
 import os
 import re
+import sys
+import json
 
 
 def get_logs_arr():
@@ -25,6 +27,10 @@ def get_parse_log_and_safe():
     client_errors = []
     server_errors = []
     the_longest_requests = []
+    is_json = False
+
+    if len(sys.argv) > 1 and sys.argv[1] == 'json':
+        is_json = True
 
     arr = get_logs_arr()
 
@@ -74,24 +80,50 @@ def get_parse_log_and_safe():
     client_errors = sorted(client_errors, key=lambda item: item[1], reverse=True)
     the_longest_requests = sorted(the_longest_requests, key=lambda item: item[3], reverse=True)
 
-    with open('python.log', 'w') as f:
-        f.write('Number of requests\n')
-        f.write(f'{str(number_of_requests)}\n')
-        f.write('Number of requests type\n')
-        for request_type in request_dict.keys():
-            f.write(f'{(request_type)} {str(request_dict[request_type])}\n')
-        f.write('Top 10 requests with the longest content length\n')
-        for i in range(min(len(the_longest_requests), 10)):
-            request = the_longest_requests[i]
-            f.write(f'{request[0]} {request[2]} {request[1]}\n')
-        f.write('Top 10 requests with client error\n')
-        for i in range(min(len(client_errors), 10)):
-            request = client_errors[i]
-            f.write(f'{request[0]} {request[2]} {request[-1].split()[0]}\n')
-        f.write('Top 10 requests with server error\n')
-        for i in range(min(len(server_errors), 10)):
-            request = server_errors[i]
-            f.write(f'{request[0]} {request[2]} {request[-1].split()[0]}\n')
+    the_longest_requests_format = []
+    client_errors_format = []
+    server_errors_format = []
+    for i in range(min(len(the_longest_requests), 10)):
+        request = the_longest_requests[i]
+        the_longest_requests_format.append(f'{request[0]} {request[2]} {request[1]}')
+
+    for i in range(min(len(client_errors), 10)):
+        request = client_errors[i]
+        client_errors_format.append(f'{request[0]} {request[2]} {request[-1].split()[0]}')
+
+    for i in range(min(len(server_errors), 10)):
+        request = server_errors[i]
+        server_errors_format.append(f'{request[0]} {request[2]} {request[-1].split()[0]}')
+
+    if not is_json:
+        with open('python.log', 'w') as f:
+            f.write('Number of requests\n')
+            f.write(f'{str(number_of_requests)}\n')
+            f.write('Number of requests type\n')
+            for request_type in request_dict.keys():
+                f.write(f'{(request_type)} {str(request_dict[request_type])}\n')
+            f.write('Top 10 requests with the longest content length\n')
+            for i in the_longest_requests_format:
+                f.write(f'{i}n')
+            f.write('Top 10 requests with client error\n')
+            for i in client_errors_format:
+                f.write(f'{i}\n')
+            f.write('Top 10 requests with server error\n')
+            for i in server_errors_format:
+                f.write(f'{i}\n')
+            return
+
+    data_dict = {
+        'Number of requests': number_of_requests,
+        'Number of requests type': request_dict,
+        'Top 10 requests with the longest content length': the_longest_requests_format,
+        'Top 10 requests with client error': client_errors_format,
+        'Top 10 requests with server error': server_errors_format,
+    }
+
+    json_dict = json.dumps(data_dict)
+    with open('python.json', 'w') as f:
+        f.write(json_dict)
 
 
 get_parse_log_and_safe()
